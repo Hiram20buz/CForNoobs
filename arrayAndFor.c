ebfa993a814dbbba0313072cd045b4bffbe23ea9
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-06-25 08:48:02 -0700

    Create arrayAndFor.c

---
+++ arrayAndFor.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a[10];
+    int k;
+    
+    for (k = 0; k < 10; k++) {
+        a[k] = k * 10;
+    } 
+
+    for (k = 0; k < 10; k++) {
+        printf("%d \n", a[k]);
+    }
+   
+	return 0;
+}
